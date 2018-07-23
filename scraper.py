7745871d59662c9fde2900fec38213fdec7fd4f2
mimorphism <bleach707@gmail.com>
2018-07-23 14:16:21 +0800

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,47 @@
+import os
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+OUTPUT_DIR = 'D:\PROJECT\PYTHON'
+
+
+def getURL(URL):
+    try:
+        html = requests.get(URL, timeout=5)
+        html.raise_for_status()
+        soup = BeautifulSoup(html.content, 'lxml')
+        return soup
+    except requests.exceptions.HTTPError as e:
+        print(e)
+    # links = soup.find_all("a", attrs={"class": "fileThumb"})
+
+
+def getFiles(soup):
+    # links = soup.find_all(href=re.compile("\.(png|jpg)$"))
+    links = soup.find_all("a", attrs={"class": "fileThumb"})
+    # links = links1.find_all(href=re.compile("\.(png|jpg)$"))
+    for link in links:
+        href = link.get('href')
+        # href = BeautifulSoup.find_all(href=re.compile("\.(png|jpg)$"))
+        # links = soup.find_all("a", attrs={"class": "fileThumb"})
+        try:
+            href = 'http:' + href
+            filename = os.path.join(OUTPUT_DIR, href.rsplit('/', 1)[-1])
+            print("Downloading %s to %s..." % (href, filename))
+            urllib.request.urlretrieve(href, filename)
+            print("Done.")
+        except urllib.error.URLError as err:
+            print("Done downloading all medias")
+
+
+def main():
+    print("Enter the URL of the thread: ")
+    bbb = getURL(input())
+    getFiles(bbb)
+
+
+if __name__ == "__main__":
+    main()
